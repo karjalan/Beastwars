89c6a1c4898960299b2935b7b8e35f70b130c702
Julian Gommans <julian.s.gommans@gmail.com>
2014-10-31 14:18:41 +1300

    Update beastwars.rb

--- beastwars.rb
+++ beastwars.rb
@@ -17,9 +17,7 @@ class Moves
 end
 end
     
-
-
-character = Bear.new(@heatlh,@attack,@defense,@speed)
+character = Bear.new
 char_h = character.stats[0]
 char_a = character.stats[1]
 char_d = character.stats[2]
@@ -29,33 +27,45 @@ puts "Attack = #{char_a}"
 puts "Defense = #{char_d}"
 puts "Speed = #{char_s}"
 
-opponent = Bear.new(@heatlh,@attack,@defense,@speed)
-opp_h = character.stats[0]
-opp_a = character.stats[1]
-opp_d = character.stats[2]
-opp_s = character.stats[3]
+opponent = Bear.new
+opp_h = opponent.stats[0]
+opp_a = opponent.stats[1]
+opp_d = opponent.stats[2]
+opp_s = opponent.stats[3]
 
-puts "Your health is #{char_h}, your opponents health is #{opp_h}"
-
+claw = Proc.new {
+    opp_h -= (20*char_a/opp_d)
+        puts "Your opponenets health is now #{opp_h}"
+    opp_d -= (opp_d*0.2).floor
+        puts "Your opponenets defense is now #{opp_d}"}
+        
+bite = Proc.new {
+    opp_h -= (30*char_a/opp_d)
+        puts "Your opponenets  health is now #{opp_h}"}
+            
+jab = Proc.new {
+    opp_h -= (10*char_a/opp_d)
+        puts "Your opponenets  health is now #{opp_h}"}
+        
 loop do
-puts "What action would you like to perform? (Claw, Bite, Jab or exit)"
+
+if opp_h <= 0
+    puts "Congradulations you have defeated your opponent."
+    break
+        elsif char_h <= 0
+        puts "You have been defeated, better luck next time"
+        break
+    end
+    
+    puts "What action would you like to perform? (Claw, Bite, Jab or exit)"
+
 action = gets.chomp.downcase
     if action == "claw"
-        opp_h -= (20*char_a/opp_d)
-        puts "Your opponenets health is now #{opp_h}"
-        opp_d -= 2
-        puts "Your opponenets defense is now #{opp_d}"
+        claw.call
         elsif action == "bite"
-            opp_h -= (30*char_a/opp_d)
-            puts "Your opponenets  health is now #{opp_h}"
+            bite.call
             elsif action == "Jab"
-                opp_h -= (10*char_a/opp_d)
-                puts "Your opponenets  health is now #{opp_h}" 
+                jab.call 
     else break
 end
 end
-
-
-
-
-    
